e7097936dffe277c8c5240bf7aa8a10341169761
ISHIMWE Jean Baptiste <103334815+hbapte@users.noreply.github.com>
2023-02-12 13:53:53 +0200

    #

--- pages/index.js
+++ pages/index.js
@@ -2,8 +2,8 @@ import Link from 'next/link';
 import Image from 'next/image';
 import { Flex, Box, Text, Button } from '@chakra-ui/react';
 
-
-import { baseUrl, fetchApi } from '../utils/fetchApi';
+import { baseUrl, fetchApi } from '../utils/fetchApi' 
+  
 
 export const Banner = ({ purpose, title1, title2, desc1, desc2, buttonText, linkName, imageUrl }) => (
   <Flex flexWrap='wrap' justifyContent='center' alignItems='center' m='10'>
@@ -13,7 +13,7 @@ export const Banner = ({ purpose, title1, title2, desc1, desc2, buttonText, link
       <Text fontSize='3xl' fontWeight='bold'>{title1}<br />{title2}</Text>
       <Text fontSize='lg' paddingTop='3' paddingBottom='3' color='gray.700'>{desc1}<br />{desc2}</Text>
       <Button fontSize='xl' bg="blue.300" color="white">
-        <Link href={linkName}><a>{buttonText}</a></Link>
+        <Link href={linkName}>{buttonText}</Link>
       </Button>
     </Box>
   </Flex> 
@@ -32,11 +32,12 @@ const Home = ({ propertiesForSale, propertiesForRent }) => (
       linkName='/search?purpose=for-rent'
       imageUrl='https://bayut-production.s3.eu-central-1.amazonaws.com/image/145426814/33973352624c48628e41f2ec460faba4'
     />
-    <Flex flexWrap='wrap'>
-      {propertiesForRent.map((property) => <Property property={property} key={property.id} />)}
+
+<Flex flexWrap='wrap'>
+      {propertiesForRent.map((property) => <property property={property} key={property.id} />)}
     </Flex>
     <Banner
-      purpose='BUY A HOME'
+      purpose='BUY A  HOME'
       title1=' Find, Buy & Own Your'
       title2='Dream Home'
       desc1=' Explore from Apartments, land, builder floors,'
@@ -45,9 +46,9 @@ const Home = ({ propertiesForSale, propertiesForRent }) => (
       linkName='/search?purpose=for-sale'
       imageUrl='https://bayut-production.s3.eu-central-1.amazonaws.com/image/110993385/6a070e8e1bae4f7d8c1429bc303d2008'
     />
-    <Flex flexWrap='wrap'>
-      {propertiesForSale.map((property) => <Property property={property} key={property.id} />)}
-    </Flex>
+     <Flex flexWrap='wrap'>
+      {propertiesForSale.map((property) => <property property={property} key={property.id} />)}
+    </Flex> 
   </Box>
 );
 
--- utils/fetchApi.js
+++ utils/fetchApi.js
@@ -5,10 +5,10 @@ export const baseUrl = 'https://bayut.p.rapidapi.com';
 export const fetchApi = async (url) => {
   const { data } = await axios.get((url), {
     headers: {
-      'x-rapidapi-host': 'bayut.p.rapidapi.com',
-      'x-rapidapi-key': process.env.NEXT_PUBLIC_RAPID_API_KEY ,
-    },
+        'X-RapidAPI-Key': 'cefcf15aa6mshb3f12285cb32e02p1baab8jsn69dae370c41b',
+        'X-RapidAPI-Host': 'bayut.p.rapidapi.com'
+      }
   });
     
   return data;
-}
+} 
