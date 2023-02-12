277a216f2c190d50eabe9a76692596f677f78c83
ISHIMWE Jean Baptiste <103334815+hbapte@users.noreply.github.com>
2023-02-12 08:32:30 +0200

    #

--- utils/fetchApi.js
+++ utils/fetchApi.js
@@ -1,6 +1,6 @@
 import axios from 'axios';
 
-const baseUrl = 'bayut.p.rapidapi.com'
+export const baseUrl = 'bayut.p.rapidapi.com'
 
 // headers: {
 // 'X-RapidAPI-Key': 'cefcf15aa6mshb3f12285cb32e02p1baab8jsn69dae370c41b',
@@ -15,4 +15,5 @@ export const fetchApi = async (url) => {
           }
     }
     )
+    return data; 
 }
